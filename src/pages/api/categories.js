750dca3a6213ca929da080c0fc31ab1eb7158a8a
kuba201 <kua.olszanek@gmail.com>
2024-04-23 12:37:45 +0200

    v1 api

---
+++ src/pages/api/categories.js
@@ -0,0 +1,45 @@
+const getCategories = [
+  {
+    id: "DAIRY",
+    name: "Nabiał",
+  },
+  {
+    id: "FRUITS",
+    name: "Owoce",
+  },
+  {
+    id: "VEGETABLE",
+    name: "Warzywa",
+  },
+  {
+    id: "MEAT",
+    name: "Mięso",
+  },
+  {
+    id: "FISHES",
+    name: "Ryby",
+  },
+  {
+    id: "HONEY",
+    name: "Miody",
+  },
+  {
+    id: "WINE_AND_SPIRITS",
+    name: "Alkohole",
+  },
+  {
+    id: "COOKED_MEATS",
+    name: "Wędliny",
+  },
+  {
+    id: "CEREALS",
+    name: "Zboża",
+  },
+  {
+    id: "OTHER",
+    name: "Pozostałe",
+  },
+];
+export default function handler(req, res) {
+  res.status(200).json(getCategories);
+}
---
+++ src/pages/api/products/[type].js
@@ -0,0 +1,301 @@
+const data = [
+  {
+    id: "APPLE",
+    name: "Jabłka",
+    type: "FRUITS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-apple-64.png",
+  },
+  {
+    id: "BEEF",
+    name: "Wołowina",
+    type: "MEAT",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-beef-64.png",
+  },
+  {
+    id: "BEET",
+    name: "Buraki",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-beet-64.png",
+  },
+  {
+    id: "BLUEBEERY",
+    name: "Borówki",
+    type: "FRUITS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-blueberry-64.png",
+  },
+  {
+    id: "BREAD",
+    name: "Chleb",
+    type: "OTHER",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-bread-64.png",
+  },
+  {
+    id: "BROCCOLI",
+    name: "Brokuły",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-broccoli-64.png",
+  },
+  {
+    id: "CABBAGE",
+    name: "Kapusty",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-cabbage-64.png",
+  },
+  {
+    id: "CARROT",
+    name: "Marchewki",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-carrot-64.png",
+  },
+  {
+    id: "CHEESE",
+    name: "Sery",
+    type: "DAIRY",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-cheese-64.png",
+  },
+  {
+    id: "CHERRY",
+    name: "Wiśnia",
+    type: "FRUITS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-cherry-64.png",
+  },
+  {
+    id: "CHILI_PEPPER",
+    name: "Ostre papryki",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-chili-pepper-64.png",
+  },
+  {
+    id: "CORN",
+    name: "Kukurydza",
+    type: "CEREALS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-corn-64.png",
+  },
+  {
+    id: "CUCUMBER",
+    name: "Ogórki",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-cucumber-64.png",
+  },
+  {
+    id: "EDAMAME_BEANS",
+    name: "Soja",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-edamame-beans-64.png",
+  },
+  {
+    id: "EGGPLANT",
+    name: "Bakłażan",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-eggplant-64.png",
+  },
+  {
+    id: "EGG",
+    name: "Jajka",
+    type: "OTHER",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-eggs-64.png",
+  },
+  {
+    id: "FISH",
+    name: "Ryby",
+    type: "FISHES",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-fish-food-64.png",
+  },
+  {
+    id: "FLOUR",
+    name: "Mąka",
+    type: "OTHER",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-flour-64.png",
+  },
+  {
+    id: "GARLIC",
+    name: "Czosnek",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-garlic-64.png",
+  },
+  {
+    id: "GRAPES",
+    name: "Winogrona",
+    type: "FRUITS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-grapes-64.png",
+  },
+  {
+    id: "HONEY",
+    name: "Miód",
+    type: "HONEY",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-honey-64.png",
+  },
+  {
+    id: "LEEK",
+    name: "Pory",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-leek-64.png",
+  },
+  {
+    id: "MILK",
+    name: "Mleko",
+    type: "DAIRY",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-milk-bottle-64.png",
+  },
+  {
+    id: "PAPRIKA",
+    name: "Papryka",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-paprika-64.png",
+  },
+  {
+    id: "PEAR",
+    name: "Gruszki",
+    type: "FRUITS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-pear-64.png",
+  },
+  {
+    id: "PLUM",
+    name: "Śliwki",
+    type: "FRUITS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-plum-64.png",
+  },
+  {
+    id: "PORK",
+    name: "Wieprzowina",
+    type: "MEAT",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-pork-64.png",
+  },
+  {
+    id: "POTATO",
+    name: "Ziemniaki",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-potato-64.png",
+  },
+  {
+    id: "RADISH",
+    name: "Rzodkiewka",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-radish-64.png",
+  },
+  {
+    id: "RASPBERRY",
+    name: "Mailny",
+    type: "FRUITS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-raspberry-64.png",
+  },
+  {
+    id: "SAUSAGES",
+    name: "Kiełbasy",
+    type: "COOKED_MEATS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-sausages-64.png",
+  },
+  {
+    id: "SPINACH",
+    name: "Szpinak",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-spinach-64.png",
+  },
+  {
+    id: "STEAK",
+    name: "Stek",
+    type: "MEAT",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-steak-64.png",
+  },
+  {
+    id: "STRAWBERRY",
+    name: "Truskawki",
+    type: "FRUITS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-strawberry-64.png",
+  },
+  {
+    id: "WHITE_BEANS",
+    name: "Biała fasola",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-white-beans-64.png",
+  },
+  {
+    id: "YOGURT",
+    name: "Jogurt",
+    type: "DAIRY",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-yogurt-64.png",
+  },
+  {
+    id: "ZUCCHINI",
+    name: "Cukinia",
+    type: "VEGETABLE",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-zucchini-64.png",
+  },
+  {
+    id: "OATS",
+    name: "Owies",
+    type: "CEREALS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-oats-64.png",
+  },
+  {
+    id: "WHEAT",
+    name: "Pszenica",
+    type: "CEREALS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-wheat-64.png",
+  },
+  {
+    id: "RYE",
+    name: "Pszenica",
+    type: "CEREALS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-flour-of-rye-64.png",
+  },
+  {
+    id: "BARLEY",
+    name: "Jęczmień",
+    type: "CEREALS",
+    iconUrl:
+      "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-flour-of-rye-64.png",
+  },
+];
+export default function handler(req, res) {
+  const { type } = req.query; // This extracts the 'type' parameter from the URL query
+  const itemsByType = data.filter(
+    (item) => item.type.toLowerCase() === type.toLowerCase()
+  );
+
+  if (itemsByType.length > 0) {
+    res.status(200).json(itemsByType); // Send all matching items as JSON
+  } else {
+    res.status(404).json({ message: "No items found for this type" }); // Send error if no matches
+  }
+}
